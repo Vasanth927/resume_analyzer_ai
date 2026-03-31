e76b3c6dd624e15668164d5a1073ce0a2f0a7f34
Vasanth927 <vasanthkumarbogilli@gmail.com>
2026-03-31 11:44:28 +0530

    Final Commit

---
+++ interface.py
@@ -0,0 +1,24 @@
+import streamlit as st
+
+# refer analysis.py file
+from analysis import analyze_resume
+
+st.set_page_config(page_title='CV Analyzer')
+
+st.title('RESUME ANALYZER USING AI 🤖🧠🇦🇮👾')
+
+st.subheader('''This page helps you to compare your resume with the given Job description''')
+
+st.sidebar.subheader('Drop your resume here ⬇️')
+pdf_doc = st.sidebar.file_uploader('Click here to browse', type = ['pdf'])
+
+st.sidebar.markdown('Designed by VasanthKumar Bogilli')
+st.sidebar.markdown("Github: https://github.com/Vasanth927")
+
+job_des = st.text_area('Copy and paste the JD here👉', max_chars=10000)
+
+submit = st.button('Generate Score📊')
+
+if submit:
+    with st.spinner('Getting Results...'):
+        analyze_resume(pdf_doc,job_des)
